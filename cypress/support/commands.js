b20efca80556166f19cfd0fb623ebced26e768f8
KimberlingYarihuaman <105390872+KimberlingYarihuaman@users.noreply.github.com>
2024-01-26 17:15:24 +0100

    Update commands.js
    Add the Login User command for Login exercise.

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,14 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+//LOGIN USERS
+Cypress.Commands.add('login', (username, password) => {
+    //PAGE
+    cy.visit('https://the-internet.herokuapp.com/login');
+    
+    //USERNAME 
+    cy.get('#username').should('be.visible').type(username);
+            
+    //PASSWORD
+    cy.get('#password').should('be.visible').type(password);
+
+    //SUBMIT BUTTON
+    cy.get('button').contains(' Login').should('be.visible').click({force: true})
+})
